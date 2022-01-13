6a7fe2a30586a4c0fa8d669265d1b7182a1aec17
henrygd <hank@henrygd.me>
2022-01-13 15:57:20 -0800

    add redis options + wordpress install

--- README.md
+++ README.md
@@ -2,19 +2,21 @@
 
 Fork of [TrafeX/docker-wordpress](https://github.com/TrafeX/docker-wordpress). Changes:
 
-- Uses existing wordpress files
-- Healthcheck runs wp-cron (disable in wp-config.php)
+- Designed to use existing wordpress files (installs fresh copy if no files found)
+- Healthcheck runs wp-cron (disabled automatically in wp-config.php)
 - Add php8-session
 - Add php redis extension
 - Add libvips support
-- Optionally adds a few plugins:
+- Optionally installs a few highly recommended plugins:
   - [VIPS Image Editor](https://github.com/henrygd/vips-image-editor) for better image processing (libvips is baked into the image)
-  - [Redis Object Cache](https://wordpress.org/plugins/redis-cache/) (add [connection parameters](https://github.com/rhubarbgroup/redis-cache/wiki/Connection-Parameters) to wp-config.php)
+  - [Redis Object Cache](https://wordpress.org/plugins/redis-cache/) (see [docker-compose.yml](docker-compose.yml) for connection params)
   - [Disable Media Pages](https://wordpress.org/plugins/disable-media-pages/) because images don't need their own page
 
 ## Usage
 
-See [docker-compose.yml](docker-compose.yml) for an example. You should bind mount existing wordpress files on the host (migrated site or fresh install) and use an external database / redis server. Expose port 80 or use with something like cloudflare tunnel or [nginx-proxy-manager](https://github.com/jc21/nginx-proxy-manager) or [traefik](https://github.com/traefik/traefik).
+See [docker-compose.yml](docker-compose.yml) for an example. You should use an external database / redis server. Expose port 80 or use with something like cloudflare tunnel or [nginx-proxy-manager](https://github.com/jc21/nginx-proxy-manager) or [traefik](https://github.com/traefik/traefik).
+
+If you don't mount existing wordpress files, it will install a fresh copy automatically. This may take a second so don't worry if you get a 502 error. After setup, restart the container to update wp-config and install plugins.
 
 ## Info
 
@@ -34,10 +36,3 @@ Lightweight WordPress container with Nginx 1.20 & PHP-FPM 8.0 based on Alpine Li
 This image includes [wp-cli](https://wp-cli.org/) which can be used like this:
 
     docker exec <your container name> /usr/local/bin/wp --path=/usr/src/wordpress <your command>
-
-## Inspired by
-
-- https://hub.docker.com/_/wordpress/
-- https://codeable.io/wordpress-developers-intro-to-docker-part-two/
-- https://github.com/TrafeX/docker-php-nginx/
-- https://github.com/etopian/alpine-php-wordpress
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,26 +3,61 @@
 # terminate on errors
 set -e
 
+# install wordpress if necessary
+CONFIG=/usr/src/wordpress/wp-config.php
+SAMPLE=/usr/src/wordpress/wp-config-sample.php
+if [ -f "$CONFIG" ]; then
+	echo "Wordpress files found. Skipping install..."
+else
+	echo "Wordpress files do not exist. Installing..."
+	if [[ ! -f "$SAMPLE" ]]; then
+		# download & extract wordpress
+		wget -q https://wordpress.org/latest.tar.gz \
+			&& tar -xzkf latest.tar.gz -C /usr/src/ \
+			&& rm latest.tar.gz \
+			&& chown -R nobody: /usr/src/wordpress
+	fi
+	echo "*** Please restart container after Wordpress setup ***"
+	exec "$@"
+fi
+
+# disable cron - handled by healthcheck
+cd /usr/src/wordpress && wp config set DISABLE_WP_CRON true --raw
+
+# redis options
+if [ -z "$WP_REDIS_HOST" ]; then
+  echo "To use redis, please define WP_REDIS_HOST"
+else
+  cd /usr/src/wordpress && wp config set WP_REDIS_HOST "$WP_REDIS_HOST"
+  cd /usr/src/wordpress && wp config set WP_REDIS_TIMEOUT 1 --raw
+  cd /usr/src/wordpress && wp config set WP_REDIS_READ_TIMEOUT 1 --raw
+fi
+if [ -z "$WP_REDIS_DATABASE" ]; then
+  echo "To specify redis database, please define WP_REDIS_DATABASE"
+else
+  cd /usr/src/wordpress && wp config set WP_REDIS_DATABASE "$WP_REDIS_DATABASE" --raw
+fi
+
 # install plugins
 if [ "$INSTALL_PLUGINS" = "true" ] ; then
-    if [ ! "$(ls -A "/usr/src/wordpress/wp-content/plugins/vips-image-editor" 2>/dev/null)" ]; then
-        echo 'Adding plugin: vips-image-editor'
-        cd /usr/src/wordpress && wp plugin install --activate https://github.com/henrygd/vips-image-editor/releases/latest/download/vips-image-editor.zip
-    fi
+	if [ ! "$(ls -A "/usr/src/wordpress/wp-content/plugins/vips-image-editor" 2>/dev/null)" ]; then
+		echo 'Adding plugin: vips-image-editor'
+		cd /usr/src/wordpress && wp plugin install --activate https://github.com/henrygd/vips-image-editor/releases/latest/download/vips-image-editor.zip
+	fi
 
-    if [ ! "$(ls -A "/usr/src/wordpress/wp-content/plugins/redis-cache" 2>/dev/null)" ]; then
-        echo 'Adding plugin: redis-cache'
-        cd /usr/src/wordpress && wp plugin install redis-cache
-    fi
+	if [ ! "$(ls -A "/usr/src/wordpress/wp-content/plugins/redis-cache" 2>/dev/null)" ]; then
+		echo 'Adding plugin: redis-cache'
+		cd /usr/src/wordpress && wp plugin install --activate redis-cache
+	fi
 
-    if [ ! "$(ls -A "/usr/src/wordpress/wp-content/plugins/disable-media-pages" 2>/dev/null)" ]; then
-        echo 'Adding plugin: disable-media-pages'
-        cd /usr/src/wordpress && wp plugin install --activate disable-media-pages
-    fi
+	if [ ! "$(ls -A "/usr/src/wordpress/wp-content/plugins/disable-media-pages" 2>/dev/null)" ]; then
+		echo 'Adding plugin: disable-media-pages'
+		cd /usr/src/wordpress && wp plugin install --activate disable-media-pages
+	fi
 
-    chown -R nobody: /usr/src/wordpress/wp-content/plugins
+	chown -R nobody: /usr/src/wordpress/wp-content/plugins
 else
-    echo "not installing plugins..."
+	echo "Not installing plugins..."
 fi
 
 exec "$@"
