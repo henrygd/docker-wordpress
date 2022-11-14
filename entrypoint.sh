80e5998e511523b0484af12cf80935b6690dcc19
hank <hank@henrygd.me>
2022-11-14 16:02:40 -0600

    fix bash comparison

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ else
 fi
 
 # install recommended plugins
-if [ "$INSTALL_PLUGINS" = "true" ] ; then
+if [ "$INSTALL_PLUGINS" == "true" ] ; then
 	if [ ! "$(ls -A "/usr/src/wordpress/wp-content/plugins/vips-image-editor" 2>/dev/null)" ]; then
 		echo 'Adding plugin: vips-image-editor'
 		cd /usr/src/wordpress && wp plugin install --activate https://github.com/henrygd/vips-image-editor/releases/latest/download/vips-image-editor.zip
